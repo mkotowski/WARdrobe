68e63de308e429c65f697abed713311f5e7c84d6
mkotowski <mkotowski101@gmail.com>
2020-05-29 04:59:51 +0200

    Prepare quad for splash's texture

--- libs/renderer/src/Window.cpp
+++ libs/renderer/src/Window.cpp
@@ -312,41 +312,31 @@ Window::SplashScreen(GLFWwindow* mainWindow)
 
 	// set up vertex data (and buffer(s)) and configure vertex attributes
 	// ------------------------------------------------------------------
-	// add a new set of vertices to form a second triangle (a total of 6
-	// vertices); the vertex attribute configuration remains the same (still one
-	// 3-float position vector per vertex)
 	float vertices[] = {
-		// first triangle
-		-0.9f,
-		-0.5f,
-		0.0f, // left
-		-0.0f,
-		-0.5f,
-		0.0f, // right
-		-0.45f,
-		0.5f,
-		0.0f, // top
-		      // second triangle
-		0.0f,
-		-0.5f,
-		0.0f, // left
-		0.9f,
-		-0.5f,
-		0.0f, // right
-		0.45f,
-		0.5f,
-		0.0f // top
+		1.0f,  1.0f,  0.0f, // top right
+		1.0f,  -1.0f, 0.0f, // bottom right
+		-1.0f, -1.0f, 0.0f, // bottom left
+		-1.0f, 1.0f,  0.0f  // top left
 	};
-
-	unsigned int VBO, VAO;
+	unsigned int indices[] = {
+		// note that we start from 0!
+		0, 1, 3, // first Triangle
+		1, 2, 3  // second Triangle
+	};
+	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and
 	// then configure vertex attributes(s).
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(
+	  GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
@@ -356,12 +346,18 @@ Window::SplashScreen(GLFWwindow* mainWindow)
 	// safely unbind
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
+	// remember: do NOT unbind the EBO while a VAO is active as the bound element
+	// buffer object IS stored in the VAO; keep the EBO bound.
+	// glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
 	// You can unbind the VAO afterwards so other VAO calls won't accidentally
 	// modify this VAO, but this rarely happens. Modifying other VAOs requires a
 	// call to glBindVertexArray anyways so we generally don't unbind VAOs (nor
 	// VBOs) when it's not directly necessary.
 	glBindVertexArray(0);
 
+	// render
+	// ------
 	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
 
@@ -370,8 +366,8 @@ Window::SplashScreen(GLFWwindow* mainWindow)
 	glBindVertexArray(
 	  VAO); // seeing as we only have a single VAO there's no need to bind it
 	        // every time, but we'll do so to keep things a bit more organized
-	glDrawArrays(GL_TRIANGLES, 0, 6); // set the count to 6 since we're drawing 6
-	                                  // vertices now (2 triangles); not 3!
+	// glDrawArrays(GL_TRIANGLES, 0, 6);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 	// glBindVertexArray(0); // no need to unbind it every time
 
 	// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved
@@ -383,6 +379,7 @@ Window::SplashScreen(GLFWwindow* mainWindow)
 	// ------------------------------------------------------------------------
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glDeleteProgram(shaderProgram);
 
 	// glfwSwapBuffers(splashScreen);
